a747cb655d3f448e8971009178f87a2f5d8528e9
Rafnes <rafzograf@gmail.com>
2024-11-15 17:08:19 +0300

    initial commit

---
+++ src/test/java/me/dineka/pizzaLab/PizzaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dineka.pizzaLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
